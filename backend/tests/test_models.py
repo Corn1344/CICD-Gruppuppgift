b3321fbee1ce725212398745bc8256c9c77a37af
Alexander Lundgren <a.k.o.lundgren@gmail.com>
2023-11-16 10:47:07 +0100

    pylint complience

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -1,14 +1,18 @@
-import pytest
-from pingurl.models import PingData
+"""Tests for PingUrl"""
 import datetime
+from pingurl.models import PingData
+
 
 def test_ok_status():
+    """Testing a valid OK status for success"""
     date = datetime.datetime.now()
     time_sec = datetime.timedelta(weeks=40, days=84, minutes=50)
     new_obj = PingData(date, time_sec, 300)
-    assert new_obj.ok_status() 
+    assert new_obj.ok_status()
+
 
 def test_ok_status_false():
+    """Testing a valid OK status for failure"""
     date = datetime.datetime.now()
     time_sec = datetime.timedelta(weeks=40, days=84, minutes=50)
     new_obj = PingData(date, time_sec, 404)
