fdc57f089398ce976f623fc091edd73f838875a0
jonkspanda <jionte@gmail.com>
2023-11-17 15:36:55 +0100

    fixed error 404-issue

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 """runs pingurl-app"""
-from pingurl import app
+# No idea why, but putting watched_urls here fixed error 404-issue
+from pingurl import app, watched_urls
 
 if __name__ == "__main__":
     app.run(debug=True)
