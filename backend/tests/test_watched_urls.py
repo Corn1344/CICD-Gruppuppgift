0e005c23027754601063ddc9f2d3f602b528bdda
Fabian <fabian@rog.home>
2023-11-17 15:03:57 +0100

    Pylint error in tests/test_watched_urls.py should be fixed

--- backend/tests/test_watched_urls.py
+++ backend/tests/test_watched_urls.py
@@ -8,21 +8,21 @@ from app import app
 obj = watched_urls
 
 
-@pytest.fixture
-def test_client():
+@pytest.fixture(scope='function', name='flask_test')
+def flask_test_client():
     """This is for mocktest of flask application"""
     with app.test_client() as client:
         yield client
 
 
-def test_get_watched_urls_err(test_client):
+def test_get_watched_urls_err(flask_test):
     """This is a mock test that send a get request to /watched-urls/0, 
     should return error status_code = 404"""
-    response = test_client.get('/watched-urls/0')
+    response = flask_test.get('/watched-urls/0')
     assert response.status_code == 404
 
 
-def test_get_watched_urls(test_client):
+def test_get_watched_urls(flask_test):
     """This is a mock test that sends a get request to /watched-urls/1, 
     since i added an url with id 1 it should return
     status code 200"""
@@ -31,5 +31,5 @@ def test_get_watched_urls(test_client):
     new_url = models.WatchedUrl(dt1, True, 1, url)
     business.add_watched_url(new_url)
     with app.app_context():
-        response = test_client.get("/watched-urls/1")
+        response = flask_test.get("/watched-urls/1")
     assert response.status_code == 200
