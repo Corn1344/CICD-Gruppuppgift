5a2c7202515b9614c810d918e1482ba402a0a09a
lundgren98 <a.k.o.lundgren@gmail.com>
2023-11-19 17:07:38 +0100

    Removed unnecessary comment.

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,4 @@
 """runs pingurl-app"""
-# No idea why, but putting watched_urls here fixed error 404-issue
 from pingurl import app
 from pingurl import watched_urls  # pylint: disable=unused-import
 
