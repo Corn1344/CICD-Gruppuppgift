d6bf01db4789c4b9c8bb63069402ca2fb3d74ad7
lundgren98 <a.k.o.lundgren@gmail.com>
2023-11-19 17:03:26 +0100

    Pylint warns about unused-imports
    But in the case of app importing watched-urls this can be ignored.
    The functions from watched-urls must be read in order for flask to
    run them on certain HTTP requests.

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 """runs pingurl-app"""
 # No idea why, but putting watched_urls here fixed error 404-issue
-from pingurl import app, watched_urls
+from pingurl import app
+from pingurl import watched_urls  # pylint: disable=unused-import
 
 if __name__ == "__main__":
     app.run(debug=True)
