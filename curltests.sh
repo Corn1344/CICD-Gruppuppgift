f08281dd6330e0e520f2f231a9ca0183482d7601
lundgren98 <a.k.o.lundgren@gmail.com>
2023-11-20 22:03:21 +0100

    curltests.sh now has functions.
    Note: I have only tested this locally.
    The script uses som gnu bashisms, this will not work in regular sh, so
    jenkins might complain.

--- curltests.sh
+++ curltests.sh
@@ -1,55 +1,76 @@
 #!/bin/bash
 ip_addr=$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" flask_application):5000
-echo "Running API tests to ip: $ip_addr. . ."
+echo "Running API tests to ip: $ip_addr..."
 echo ""
 
-response=$(curl -s -o /dev/null --retry-delay 1 --retry 10 --retry-all-errors -w "%{http_code}" -I http://$ip_addr)
-if [[ $response == "404" ]]; then
-	echo "Running tests ..."
-else
-	echo "Connection timed out"
-	exit 1
-fi
+try_conn () {
+	res=$(curl -s -o /dev/null --retry-delay 1 --retry 10 --retry-all-errors -w "%{http_code}" -I http://$ip_addr)
+	if [[ $res == "404" ]]; then
+		echo "Running tests..."
+	else
+		echo "Connection timed out"
+		exit 1
+	fi
+}
 
-postreq=$(curl -s -X POST http://$ip_addr/watched-urls -H "Content-Type: application/json" -d '{"activateAt": "2023-11-06T01:36:28+00:00", "force": true, "periodSec": 30, "url": "https://www.youtube.com"}')
-URLID=$(echo $postreq | grep -oE "[0-9]+")
-if [[ $postreq == *'message'* ]]; then
-	echo "POST request succeded"
-else
-	echo "POST request failed"
-	exit 1
-fi
+assert () {
+	if [[ $1 == *"$2"* ]]; then
+		echo "OK!"
+	else
+		echo "FAILED!"
+		exit 1
+	fi
+}
 
-getreq=$(curl -s -X GET http://$ip_addr/watched-urls)
-if [[ $getreq == *'urlIds'* ]]; then
-	echo "GET request succeded"
-else
-	echo "GET request failed"
-	exit 1
-fi
+test_watched_url_post () {
+	echo -n "$FUNCNAME..."
+	URL=http://$ip_addr/watched-urls
+	JSON='''
+	{
+		"activateAt": "2023-11-06T01:36:28+00:00",
+		"force": true,
+		"periodSec": 30,
+		"url": "https://www.youtube.com"
+	}
+	'''
+	res=$(curl -s -X POST $URL -H "Content-Type: application/json" -d "$JSON")
+	assert "$res" "message"
+	URLID=$(echo $res | grep -oE "[0-9]+")
+}
 
-getreqid=$(curl -s -X GET http://$ip_addr/watched-urls/$URLID)
-if [[ $getreqid == *'activateAt'* ]]; then
-	echo "GET request with ID succeded"
-else
-	echo "GET request with ID failed"
-	exit 1
-fi
+test_watched_url_get () {
+	echo -n "$FUNCNAME..."
+	URL=http://$ip_addr/watched-urls
+	res=$(curl -s -X GET $URL)
+	assert "$res" "urlIds"
+}
 
-delreqid=$(curl -s -X DELETE http://$ip_addr/watched-urls/$URLID)
-if [[ $delreqid == *'message'* ]]; then
-	echo "DELETE request with ID succeded"
-else
-	echo "DELETE request with ID failed"
-	exit 1
-fi
 
-delreqid=$(curl -s -X DELETE http://$ip_addr/watched-urls/$URLID)
-if [[ $delreqid == *'error'* ]]; then
-	echo "DELETE request attempt 2 successful failure"
-else
-	echo "DELETE request attempt 2 failure"
-	exit 1
-fi
+test_watched_url_id_get () {
+	echo -n "$FUNCNAME..."
+	URL=http://$ip_addr/watched-urls/$URLID
+	res=$(curl -s -X GET $URL)
+	assert "$res" "activateAt"
+}
+
+test_watched_url_id_delete () {
+	echo -n "$FUNCNAME..."
+	URL=http://$ip_addr/watched-urls/$URLID
+	res=$(curl -s -X DELETE $URL)
+	assert "$res" "message"
+}
+
+test_watched_url_id_delete_should_err () {
+	echo -n "$FUNCNAME..."
+	URL=http://$ip_addr/watched-urls/$URLID
+	res=$(curl -s -X DELETE $URL)
+	assert "$res" "error"
+}
+
+test_watched_url_post
+test_watched_url_get
+test_watched_url_id_get
+test_watched_url_id_delete
+test_watched_url_id_delete_should_err
 
 exit 0
