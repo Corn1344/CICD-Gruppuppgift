3ccc1ee5d051c747281892ddb21b998aa71ee81a
JonksPanda <jionte@gmail.com>
2023-11-20 13:19:43 +0100

    added test for get_stats and fixed get_stats-function

--- backend/tests/test_persistance_business_ping_integration.py
+++ backend/tests/test_persistance_business_ping_integration.py
@@ -19,4 +19,23 @@ def test_added_url_in_get_urls():
     urls = persistance.get_url_ids()
     assert wu.url_id in urls
     assert pd.url_id in urls
-    
+
+
+def test_added_url_and_ping_in_get_stats():
+    """
+    Tests if persistance.get_stats() can receive
+    no. of urls and pings added from business.add_watched_url()
+    and ping.send_ping()
+    """
+
+    dt = datetime(2023, 11, 16, tzinfo=timezone.utc)
+    wu_list = [
+        WatchedUrl(dt, True, 1, "http://www.example.org"),
+        WatchedUrl(dt, True, 1, "http://www.example.org"),
+    ]
+    for wu in wu_list:
+        business.add_watched_url(wu)
+        pd = ping.send_ping(wu)
+        persistance.add_ping_data(pd)
+    stats = persistance.get_stats()
+    assert stats == {"watchedUrls": 2, "pings": 4}
